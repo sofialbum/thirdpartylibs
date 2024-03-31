fc0c0866bafc3897a49a283ed461ff591db918f6
Sofía González <sofia_1.6@hotmail.com>
2024-03-31 16:31:41 -0300

    typedeclarations

---
+++ dist/index.js
@@ -0,0 +1,21 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var axios_1 = __importDefault(require("axios"));
+axios_1.default
+    .get("https://jsonplaceholder.typicode.com/users/1")
+    .then(function (res) {
+    console.log("WOO!!");
+    var data = res.data;
+    printUser(res.data);
+})
+    .catch(function (e) {
+    console.log("ERROR!", e);
+});
+function printUser(user) {
+    console.log(user.name);
+    console.log(user.email);
+    console.log(user.phone);
+}
